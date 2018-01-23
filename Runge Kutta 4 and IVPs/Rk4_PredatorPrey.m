1e1c9861eb6a820dd37002907783c3046307b776
Jacob Jones <35271046+jacobjones0513@users.noreply.github.com>
2018-01-23 14:35:05 -0700

    Add files via upload

---
+++ Runge Kutta 4 and IVPs/Rk4_PredatorPrey.m
@@ -0,0 +1,28 @@
+%%Jacob Jones
+
+clear;
+
+%defining information to be passed to RK4 function
+f = @(t,y) [2*y(1)-1.2*y(1)*y(2)
+            -y(2) + 0.9*y(1)*y(2)]';
+t0 = 0;
+y0 = [2 3];
+dt = 0.05;
+tsteps = 200;
+t = t0 : dt : t0+tsteps*dt;
+
+y = RK4(f, y0, t0, dt, tsteps);
+
+figure(1)
+plot(y(:,1),y(:,2),'m-');
+xlabel('Rabbits');
+ylabel('Foxes');
+hold;
+
+figure(2)
+plot(t,y(:,1),'r-o');
+hold on;
+plot(t,y(:,2),'b-o');
+xlabel('Time');
+ylabel('Foxes and Rabbits');
+legend('Rabbits', 'Foxes')
